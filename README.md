12a7685a692cbeda95390ac91abbcee431b0f6ad
Ingrid Sena <10999758+senaingrid@users.noreply.github.com>
2020-05-05 16:12:37 +0200

    Add 1st endpoint

--- README.md
+++ README.md
@@ -2,35 +2,14 @@ Run the application from:
 
 - your IDE, by running the main method from the `OpenAPIServer` class, or
 
-- with Maven: `mvn compile exec:java`
+- with Maven: `mvn clean install && mvn exec:java`
 
 
 Then you can test your API using any command-line tool like curl:
 ```
+curl -X GET "http://127.0.0.1:3000/api" -H "accept: application/json"
+Welcome to FASTEN API
 
-Curl
-curl -X GET "http://127.0.0.1:8000/api/pypi/numpy/1.15.2" -H "accept: application/json"
-Request URL
-
-http://127.0.0.1:8000/api/pypi/numpy/1.15.2
-
-
-
-
-$ curl http://localhost:8080/pets
-[{"id":1,"name":"Fufi","tag":"ABC"},{"id":2,"name":"Garfield","tag":"ABC"},{"id":3,"name":"Puffa","tag":"ABC"}]
-
-$ curl http://localhost:8080/pets/3
-{"id":3,"name":"Puffa","tag":"ABC"}
-
-$ curl http://localhost:8080/pets/5
-{"code":404,"message":"Pet not found"}
-
-$ curl -X POST -H "Content-type: application/json" --data '{"id":4,"name":"Alan"}' http://localhost:8080/pets
-
-$ curl -X POST -H "Content-type: application/json" --data '{"id":4}' http://localhost:8080/pets
-{"code":400,"message":"$.name: is missing but it is required"}
-
-$ curl http://localhost:8080/pets
-[{"id":1,"name":"Fufi","tag":"ABC"},{"id":2,"name":"Garfield","tag":"ABC"},{"id":3,"name":"Puffa","tag":"ABC"},{"id":4,"name":"Alan"}]
+curl -X GET "http://127.0.0.1:3000/api/pypi/numpy/1.15.2" -H "accept: application/json"
+{"message":"Package manager: pypi, Product: numpy, Version: 1.15.2."}
 ```
--- src/main/java/org/example/openapiserver/OpenAPIServer.java
+++ src/main/java/org/example/openapiserver/OpenAPIServer.java
@@ -6,113 +6,135 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.api.RequestParameters;
+import io.vertx.ext.web.api.contract.RouterFactoryOptions;
 import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 
 public class OpenAPIServer extends AbstractVerticle {
 
-        HttpServer server;
-        Logger logger = LoggerFactory.getLogger("OpenAPI3RouterFactory");
+    HttpServer server;
+    Logger logger = LoggerFactory.getLogger("OpenAPI3RouterFactory");
 
+    @Override
+    public void start(Future<Void> future) {
+        OpenAPI3RouterFactory.create(this.vertx, "openapispec.json", asyncResult -> {
+            if (asyncResult.succeeded()) {
+                OpenAPI3RouterFactory routerFactory = asyncResult.result();
 
+                // Enable automatic response when ValidationException is thrown
+                routerFactory.setOptions(new RouterFactoryOptions().setMountResponseContentTypeHandler(true));
 
-        @Override
-        public void start(Future<Void> future) {
-                OpenAPI3RouterFactory.create(this.vertx, "openapispec.json", ar -> {
-                        if (ar.succeeded()) {
-                                OpenAPI3RouterFactory routerFactory = ar.result();
+                // Add routes handlers
 
-                                // Add routes handlerss
-                                // "/api/{pkg_manager}/{product}/{version}"
-                                // "api/pypi/numpy/1.12.3"
-                                routerFactory.addHandlerByOperationId("get_metadata_api__pkg_manager___product___version__get", routingContext -> {
-                                    RequestParameters params = routingContext.get("parserdParameters");
-                                    String pkg_manager = params.pathParameter("pkg_manager").getString();
-                                    String product = params.pathParameter("product").getString();
-                                    String version = params.pathParameter("version").getString();
-                                    // TODO: call a function to do the query to find the pkg_manager/product/version
-                                    String reply = dummyFunction(pkg_manager, product, version);
-                                    if (!reply.isEmpty())
-                                        routingContext
-                                            .response()
-                                            .setStatusCode(200)
-                                            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                                            .end();
-                                    else
-                                        routingContext.fail(404, new Exception("Query not found!"));
-                                        // TODO: nice to have more specifc exceptions ex: pkg not found, product not found etc.
-                                });
+                // "/"
+                routerFactory.addHandlerByOperationId("root_api__get",
+                        routingContext -> routingContext.response().setStatusCode(200)
+                                .putHeader(HttpHeaders.CONTENT_TYPE, "text/html").end("Welcome to FASTEN api")
+                // TODO: Do we need a html page for this endpoint? Or just the answer!?
+                );
 
-                                // Generate the router
-                                Router router = routerFactory.getRouter();
+                // "/api/{pkg_manager}/{product}/{version}"
+                // "api/pypi/numpy/1.12.3"
+                routerFactory.addHandlerByOperationId("get_metadata_api__pkg_manager___product___version__get",
+                        routingContext -> {
+                            RequestParameters params = routingContext.get("parsedParameters");
+                            String pkg_manager = params.pathParameter("pkg_manager").getString();
+                            String product = params.pathParameter("product").getString();
+                            String version = params.pathParameter("version").getString();
+                            System.out.println("DEBUG: Parsed parameters converted to string (" + pkg_manager + ", "
+                                    + product + ", " + version + ")."); // update this to logger.debug
+                            // TODO: call a function to do the DB query
+                            JsonObject reply = dummyFunction(pkg_manager, product, version);
+                            if (!reply.isEmpty())
+                                routingContext.response().setStatusCode(200)
+                                        .putHeader(HttpHeaders.CONTENT_TYPE, "application/json").end(reply.encode());
+                            else
+                                routingContext.fail(404, new Exception("Query not found!"));
+                            // TODO: nice to have more specifc exceptions ex: pkg not found, product not
+                            // found etc.
+                        });
 
-                                // Handle errors
-                                router.errorHandler(404, routingContext -> {
-                                        JsonObject errorObject = new JsonObject()
-                                                .put("code", 404)
-                                                .put("message",
-                                                        (routingContext.failure() != null) ?
-                                                                routingContext.failure().getMessage() :
-                                                                "Not Found"
-                                                );
-                                        routingContext
-                                                .response()
-                                                .setStatusCode(404)
-                                                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                                                .end(errorObject.encode());
-                                });
-                                router.errorHandler(400, routingContext -> {
-                                        JsonObject errorObject = new JsonObject()
-                                                .put("code", 400)
-                                                .put("message",
-                                                        (routingContext.failure() != null) ?
-                                                                routingContext.failure().getMessage() :
-                                                                "Validation Exception"
-                                                );
-                                        routingContext
-                                                .response()
-                                                .setStatusCode(400)
-                                                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                                                .end(errorObject.encode());
-                                });
+                // Generate the router
+                Router router = routerFactory.getRouter();
 
-                                server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost"));
-                                server.requestHandler(router).listen();
-
-                                future.complete(); // Complete the verticle start
-                        } else {
-                                future.fail(ar.cause()); // Fail the verticle start
-                        }
+                // Handle errors
+                router.errorHandler(404, routingContext -> {
+                    JsonObject errorObject = new JsonObject()
+                        .put("code", 404)
+                        .put("message",
+                            (routingContext.failure() != null) ?
+                                routingContext.failure().getMessage() :
+                                "Not Found");
+                    routingContext
+                        .response()
+                        .setStatusCode(404)
+                        .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .end(errorObject.encode());
                 });
-        }
+                router.errorHandler(400, routingContext -> {
+                    JsonObject errorObject = new JsonObject()
+                        .put("code", 400)
+                        .put("message",
+                            (routingContext.failure() != null) ?
+                                routingContext.failure().getMessage():
+                                "Validation Exception");
+                    routingContext
+                        .response()
+                        .setStatusCode(400)
+                        .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .end(errorObject.encode());
+                });
 
-        @Override
-        public void stop(){
-            this.server.close();
-        }
+                // Set http server
+                server = vertx.createHttpServer(new HttpServerOptions()
+                    .setPort(3000)
+                    .setHost("localhost"));
+                server.requestHandler(router).listen(serverAsyncResult -> {
+                    if (serverAsyncResult.succeeded()) {
+                        logger.info("Server started on port "
+                            + serverAsyncResult.result().actualPort());
+                    } else {
+                        logger.info(serverAsyncResult.cause());
+                    }
+                });
+                // Complete the verticle start
+                future.complete();
+                logger.info("Verticle start completed");
+            } else {
+                // Something went wrong during router factory initialization
+                future.fail(asyncResult.cause());
+                logger.info("Verticle failed");
+            }
+        });
+    }
 
-        public static void main(String[] args) {
-            Vertx vertx = Vertx.vertx();
-            vertx.deployVerticle(new OpenAPIServer());
-        }
+    @Override
+    public void stop(Future<Void> future) {
+        logger.info("\n Closing server");
+        this.server.close();
+    }
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new OpenAPIServer());
+        System.out.println("Deployed");
+    }
 
-        private String dummyFunction(String pkg_manager, String product, String version) {
-            return "It's working";
-        }
+    private JsonObject dummyFunction(String pkg_manager, String product, String version) {
+            String msg = "Package manager: "+pkg_manager+", Product: "+product+", Version: "+version+".";
 
-        /* private List<JsonObject> getMetadata() {
-            return this.metadataResp;
-        } */
+            JsonObject result = new JsonObject().put("message", msg);
+
+            logger.info(result);
+
+            return result;
+    }
+
+    /* private List<JsonObject> getMetadata() {
+        return this.metadataResp;
+    } */
 
 }
